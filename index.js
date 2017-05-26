43231ec1dfbad5a9dedfbbfa895f0aab9f396742
JobbyM <songzhipengmune@gmail.com>
2017-05-26 14:34:08 +0800

    功能设计

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 var path = require('path')
 var express = require('express')
+var session = require('express-session')
+var mongoStore = require('connect-mongo')(session)
+var flash = require('connect-flash')
+var config = require('config-lite')(__dirname)
+var routes = require('./routes')
+var pkg = require('./package')
+
+
 var app = express();
-var indexRouter = require('./routes/index')
-var userRouter = require('./routes/users')
 
-app.set('views', path.join(__dirname, 'views')) // 设置存放模块文件的目录
-app.set('view engine', 'ejs') // 设置模块引擎为ejs
+// 设置存放模块文件的目录
+app.set('views', path.join(__dirname, 'views'))
+// 设置模块引擎为ejs
+app.set('view engine', 'ejs')
 
-app.use('/', indexRouter)
-app.use('/users', userRouter)
 
-app.listen(3000)
+// 设置静态文件目录
+app.use(session({
+  name: config.session.key, // 设置cookie 中保存session id 的字段名称
+  secret: config.session.secret, // 通过设置secret 来计算hash 值并放在cookie 中，使产生的signedCookie 防篡改
+  resave: true, // 强制更新 session
+  saveUninitialized: false, // 设置为false， 强制创建一个session， 即使用户未登录
+  cookie: {
+    maxAge: config.session.maxAge // 过期时间，过期后cookie 中的session id 自动删除
+  },
+  store: new mongoStore({// 将session 存储到mongodb
+    url: config.mongodb // mongodb 地址
+  })
+}))
+
+// flash 中间件，用来显示通知
+app.use(flash())
+
+// 路由
+routes(app)
+
+// 监听端口，启动程序
+app.listen(config.port, function(){
+  console.log(`${pkg.name} listening on port ${config.port}`)
+})
---
+++ routes/posts.js
@@ -0,0 +1,53 @@
+var express = require('express')
+var router = express.Router()
+
+var checkLogin = require('../middlewares/check').checkLogin
+
+// GET /posts 所有用户或特定用户的文章页
+// GET /posts?author=xxx
+router.get('/', function(req, res, next){
+  res.send(req.flash())
+})
+
+// POST /posts 发表一篇文章
+router.post('/', checkLogin, function(req, res, next){
+  res.send(req.flash())
+})
+
+// GET /posts/create 发表文章页
+router.get('/create', checkLogin, function(req, res, next){
+  res.send(req.flash())
+})
+
+// GET /posts/:postId 单独一篇的文章页
+router.get('/:postId', checkLogin, function(req, res, next){
+  res.send(req.flash())
+})
+
+// GET /posts/:postId/edit 更新文章页
+router.get('/:postId/edit', checkLogin, function(req, res, next){
+  res.send(req.flash())
+})
+
+// GOST /posts/:postId/edit 更新一篇文章
+router.post('/:postId/edit', checkLogin, function(req, res, next){
+  res.send(req.flash())
+})
+
+// GET /posts/:postId/remove 删除一篇文章
+router.post('/:postId/remove', checkLogin, function(req, res, next){
+  res.send(req.flash())
+})
+
+// POST /posts/:postId/comment 创建一条留言
+router.get('/:postId/comment', checkLogin, function(req, res, next){
+  res.send(req.flash())
+})
+
+// GET /posts/:postId/comment/:commmentId/remove 删除一条留言
+router.get('/:postId/comment/:commmentId/remove', checkLogin, function(req, res, next){
+  res.send(req.flash())
+})
+
+
+module.exports = router
