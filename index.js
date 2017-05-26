2c5b13ff7fa5faedb614a307f404bd23654d20bb
JobbyM <songzhipengmune@gmail.com>
2017-05-26 09:30:35 +0800

    使用模块引擎ejs，users.ejs

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var path = require('path')
 var express = require('express')
 var app = express();
 var indexRouter = require('./routes/index')
 var userRouter = require('./routes/users')
+
+app.set('views', path.join(__dirname, 'views')) // 设置存放模块文件的目录
+app.set('view engine', 'ejs') // 设置模块引擎为ejs
 
 app.use('/', indexRouter)
 app.use('/users', userRouter)
